b0b3ce5a0f11c07f1d48b6c8abb54d2cdd1f2ae6
jimmy057 <jimmypolanco2540@gmail.com>
2026-02-12 21:26:17 -0400

    arregando los nombre en la capa del domain

--- app/src/main/java/com/example/jimmy_polanco_ap2_p1_/domain/model/Cerveza.kt
+++ app/src/main/java/com/example/jimmy_polanco_ap2_p1_/domain/model/Cerveza.kt
@@ -1,8 +1,8 @@
 package com.example.jimmy_polanco_ap2_p1_.domain.model
 
 data class Cerveza(
-    val IdCerveza: Int =0,
-    val Nombre: String,
-    val Marca: String,
-    val Puntuacion: Int
+    val idCerveza: Int = 0,
+    val nombre: String,
+    val marca: String,
+    val puntuacion: Int
 )

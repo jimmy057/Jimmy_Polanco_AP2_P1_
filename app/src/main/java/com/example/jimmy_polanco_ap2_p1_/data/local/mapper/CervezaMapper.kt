59de2ccbb890730d9538add8b9a5efc4b2e3cf67
jimmy057 <jimmypolanco2540@gmail.com>
2026-02-12 21:27:08 -0400

    arreglando el mapper

--- app/src/main/java/com/example/jimmy_polanco_ap2_p1_/data/local/mapper/CervezaMapper.kt
+++ app/src/main/java/com/example/jimmy_polanco_ap2_p1_/data/local/mapper/CervezaMapper.kt
@@ -4,15 +4,15 @@ import com.example.jimmy_polanco_ap2_p1_.data.local.entities.CervezaEntity
 import com.example.jimmy_polanco_ap2_p1_.domain.model.Cerveza
 
 fun CervezaEntity.toDomain() = Cerveza(
-    IdCerveza,
-    Nombre,
-    Marca,
-    Puntuacion
+    idCerveza = idCerveza,
+    nombre = nombre,
+    marca = marca,
+    puntuacion = puntuacion
 )
 
 fun Cerveza.toEntity() = CervezaEntity(
-    IdCerveza,
-    Nombre,
-    Marca,
-    Puntuacion
+    idCerveza = idCerveza,
+    nombre = nombre,
+    marca = marca,
+    puntuacion = puntuacion
 )

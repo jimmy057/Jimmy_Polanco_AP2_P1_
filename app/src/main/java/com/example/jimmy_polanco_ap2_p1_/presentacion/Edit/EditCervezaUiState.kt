3fc54d87a2ee6c7d2df4c54dd11a32edc36dc12b
jimmy057 <jimmypolanco2540@gmail.com>
2026-02-12 21:33:04 -0400

    agregando el edit uistate

---
+++ app/src/main/java/com/example/jimmy_polanco_ap2_p1_/presentacion/Edit/EditCervezaUiState.kt
@@ -0,0 +1,8 @@
+package com.example.jimmy_polanco_ap2_p1_.presentacion.Edit
+
+data class EditCervezaUiState(
+    val id: Int = 0,
+    val nombre: String = "",
+    val marca: String = "",
+    val puntuacion: String = ""
+)
